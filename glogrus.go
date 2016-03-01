544444beffd93a4cba5c62aafa1c533f164a0417
Jonathan Doklovic <doklovic@atlassian.com>
2016-03-01 15:42:33 -0600

    simplification

--- glogrus.go
+++ glogrus.go
@@ -4,32 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
 	"github.com/Sirupsen/logrus"
 	"goji.io"
 	"golang.org/x/net/context"
 )
 
-// glogrus is a middleware handler that logs the
-// request and response in a structured way
-type glogrus struct {
-	h    goji.Handler
-	c    context.Context
-	l    *logrus.Logger
-	name string
-}
-
 // NewGlogrus allows you to configure a goji middleware that logs all requests and responses
 // using the structured logger logrus. It takes the logrus instance and the name of the app
-// as the parameters and returns a middleware of type "func(c *web.C, http.Handler) http.Handler"
+// as the parameters and returns a middleware of type "func(goji.Handler) goji.Handler"
 //
 // Example:
 //
 //		package main
 //
 //		import(
-//			"github.com/zenazn/goji"
-//			"github.com/zenazn/goji/web/middleware"
+//			""goji.io"
 //			"github.com/goji/glogrus"
 //			"github.com/Sirupsen/logrus"
 //		)
@@ -48,6 +37,37 @@ func NewGlogrus(l *logrus.Logger, name string) func(goji.Handler) goji.Handler {
 	return NewGlogrusWithReqId(l, name, emptyRequestId)
 }
 
+// NewGlogrusWithReqId allows you to configure a goji middleware that logs all requests and responses
+// using the structured logger logrus. It takes the logrus instance, the name of the app and a function
+// that can retrieve a requestId from the Context "func(context.Context) string"
+// as the parameters and returns a middleware of type "func(goji.Handler) goji.Handler"
+//
+// Passing in the function that returns a requestId allows you to "plug in" other middleware that may set the request id
+//
+// Example:
+//
+//		package main
+//
+//		import(
+//			""goji.io"
+//			"github.com/goji/glogrus"
+//			"github.com/Sirupsen/logrus"
+//		)
+//
+//		func main() {
+//
+//			logr := logrus.New()
+//			logr.Formatter = new(logrus.JSONFormatter)
+//			goji.Use(glogrus.NewGlogrusWithReqId(logr, "my-app-name", GetRequestId))
+//
+//			goji.Get("/ping", yourHandler)
+//			goji.Serve()
+//		}
+//
+//		func GetRequestId(ctx context.Context) string {
+//			return ctx.Value("requestIdKey")
+//		}
+//
 func NewGlogrusWithReqId(l *logrus.Logger, name string, reqidf func(context.Context) string) func(goji.Handler) goji.Handler {
 	return func(h goji.Handler) goji.Handler {
 		fn := func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
