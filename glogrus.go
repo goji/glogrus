666f1f2616217cf8cb3321a17408150a66384a54
Jonathan Doklovic <doklovic@atlassian.com>
2016-03-01 15:07:32 -0600

    removing old code

--- glogrus.go
+++ glogrus.go
@@ -19,36 +19,6 @@ type glogrus struct {
 	name string
 }
 
-func (glogr glogrus) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	start := time.Now()
-
-	//TODO: figure out how to get a proper reqId in the context
-	//reqID := middleware.GetReqID(*glogr.c)
-
-	glogr.l.WithFields(logrus.Fields{
-		//"req_id": reqID,
-		"uri":    req.RequestURI,
-		"method": req.Method,
-		"remote": req.RemoteAddr,
-	}).Info("req_start")
-	lresp := wrapWriter(resp)
-
-	glogr.h.ServeHTTPC(glogr.c, lresp, req)
-	lresp.maybeWriteHeader()
-
-	latency := float64(time.Since(start)) / float64(time.Millisecond)
-
-	glogr.l.WithFields(logrus.Fields{
-		//"req_id":  reqID,
-		"status":  lresp.status(),
-		"method":  req.Method,
-		"uri":     req.RequestURI,
-		"remote":  req.RemoteAddr,
-		"latency": fmt.Sprintf("%6.4f ms", latency),
-		"app":     glogr.name,
-	}).Info("req_served")
-}
-
 // NewGlogrus allows you to configure a goji middleware that logs all requests and responses
 // using the structured logger logrus. It takes the logrus instance and the name of the app
 // as the parameters and returns a middleware of type "func(c *web.C, http.Handler) http.Handler"
